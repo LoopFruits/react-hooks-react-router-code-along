6f384c4242c3e02061deb3c865518db21b79655c
LoopFruits <Kensworldc@gmail.com>
2022-05-13 15:51:31 -0400

    refactored components

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import About from "./About";
-import Login from "./Login";
-import Home from "./Home";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-
-
-function App() {
-  return (
-    <>
-  <Switch>
-  <Route path="/about">
-    <About />
-  </Route>
-  <Route path="/login">
-    <Login />
-  </Route>
-  <Route exact path="/">
-    <Home />
-  </Route>
-  </Switch>
-
-  </>
-  );
-}
-
-
+import { BrowserRouter} from "react-router-dom";
+import App from "./App";
 
 
 
